4b15dd6fa4ae1ddcc42c24a56b7305527e9094e6
Tahidur-Rahman <mdtahidurrahman20@gmail.com>
2021-08-01 01:26:27 +0600

    animation 2

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../containers/HomeScreen';
+import AnimationTwo from '../containers/AnimationTwo';
 const {Navigator, Screen} = createStackNavigator();
 
 const AppStack = () => {
  
     return (
       <Navigator>
+        <Screen component={AnimationTwo} name="AnimationTwo"  options={{headerShown:false}}/>
         <Screen component={HomeScreen} name="HomeScreen"  options={{headerShown:false}}/>
       </Navigator>
     );
